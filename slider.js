de9edf8910b55664dadd925c9f09c3145490b6af
Yanis Shirokikh <yan4on@yandex.ru>
2021-01-26 07:52:34 +0300

    добавил свайп в шестеренку

--- slider.js
+++ slider.js
@@ -23,23 +23,66 @@ if (typeof mechanismMobDescription.textContent !== "undefined") {
 }
 
 
-  btnRight.addEventListener('click', function() {
-  var current = +mechanismMobDescription.getAttribute('data-current'); // запоминаем текущий отображаемый элемент в атрибуте
-  current++;
-  if (current >= sliderText.length) {
-    current = 0;
-  }
-  mechanismMobDescription.setAttribute('data-current', current);
-  mechanismMobDescription.innerHTML = sliderText[current];
-});
+//   btnRight.addEventListener('click', function() {
+//   var current = +mechanismMobDescription.getAttribute('data-current'); // запоминаем текущий отображаемый элемент в атрибуте
+//   current++;
+//   if (current >= sliderText.length) {
+//     current = 0;
+//   }
+//   mechanismMobDescription.setAttribute('data-current', current);
+//   mechanismMobDescription.innerHTML = sliderText[current];
+// });
 
-btnLeft.addEventListener('click', function() {
-  var current = +mechanismMobDescription.getAttribute('data-current');
-  current--;
-  if (current < 0) {
-    current = sliderText.length - 1;
-  }
-  mechanismMobDescription.setAttribute('data-current', current);
-  mechanismMobDescription.innerHTML = sliderText[current];
-});
+// btnLeft.addEventListener('click', function() {
+//   var current = +mechanismMobDescription.getAttribute('data-current');
+//   current--;
+//   if (current < 0) {
+//     current = sliderText.length - 1;
+//   }
+//   mechanismMobDescription.setAttribute('data-current', current);
+//   mechanismMobDescription.innerHTML = sliderText[current];
+// });
 
+function swipeLeft() {
+    var current = +mechanismMobDescription.getAttribute('data-current');
+    current--;
+    if (current < 0) {
+      current = sliderText.length - 1;
+    }
+    mechanismMobDescription.setAttribute('data-current', current);
+    mechanismMobDescription.innerHTML = sliderText[current];
+  }
+
+  function swipeRigth() {
+    var current = +mechanismMobDescription.getAttribute('data-current'); // запоминаем текущий отображаемый элемент в атрибуте
+    current++;
+    if (current >= sliderText.length) {
+      current = 0;
+    }
+    mechanismMobDescription.setAttribute('data-current', current);
+    mechanismMobDescription.innerHTML = sliderText[current];
+  }
+
+
+var initialPoint;
+var finalPoint;
+document.addEventListener('touchstart', function(event) {
+event.preventDefault();
+event.stopPropagation();
+initialPoint=event.changedTouches[0];
+}, false);
+document.addEventListener('touchend', function(event) {
+event.preventDefault();
+event.stopPropagation();
+finalPoint=event.changedTouches[0];
+var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+var yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
+if (xAbs > 20 || yAbs > 20) {
+if (xAbs > yAbs) {
+if (finalPoint.pageX < initialPoint.pageX){
+    swipeLeft()}
+else{
+    swipeRigth()}
+    }
+}
+}, false);
